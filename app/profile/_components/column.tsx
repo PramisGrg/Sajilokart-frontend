a68140c909fcfe3c3fdaf6bc83472d94b3d9c16c
PramisGrg <gurungpramis361@gmail.com>
2025-03-01 17:03:38 +0545

    feat: product table actions

--- app/profile/_components/column.tsx
+++ app/profile/_components/column.tsx
@@ -1,19 +1,97 @@
 "use client";
+import ImageWrapper from "@/components/common/image-wrapper";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export type TProductType = {
   id: string;
   name: string;
-  //   image: string;
+  image: string | undefined;
   description: string;
   price: number;
   stock: number;
+};
+
+const ActionsCell = ({ productId }: { productId: string }) => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="border-none bg-white">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => router.push(`product/${productId}`)}>
+            Edit Product
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            Delete Product
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-md border-none">
+          <DialogHeader>
+            <DialogTitle>Delete Product</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete the product ?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+            <Button className="bg-red-400">Delete</Button>
+            <DialogClose asChild>
+              <Button className="text-white" type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export const columns: ColumnDef<TProductType>[] = [
   {
     accessorKey: "name",
     header: "Product",
+    cell: ({ row }) => {
+      return (
+        <div className="flex gap-4 items-center">
+          <ImageWrapper
+            className="w-10 h-10 rounded-lg"
+            image={row.original.image}
+          />
+          <h1>{row.original.name}</h1>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "price",
@@ -26,5 +104,13 @@ export const columns: ColumnDef<TProductType>[] = [
   {
     accessorKey: "description",
     header: "Description",
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const productId = row.original.id;
+      return <ActionsCell productId={productId} />;
+    },
   },
 ];
---
+++ app/profile/product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditPage = () => {
+  return (
+    <div>
+      <h1>This is edit page</h1>
+    </div>
+  );
+};
+
+export default EditPage;
